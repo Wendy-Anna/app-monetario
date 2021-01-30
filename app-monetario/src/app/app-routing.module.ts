27671f089e626d5117bfed0f341b9dd69c856942
NaionaraRamos <nramosmaceda@gmail.com>
2021-01-30 12:14:50 -0300

    Adicionando rotas

--- app-monetario/src/app/app-routing.module.ts
+++ app-monetario/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { CadastroUsuarioComponent } from './cadastro-usuario/cadastro-usuario.component';
+import { LoginUsuarioComponent } from './login-usuario/login-usuario.component';
+import { CadastroContaComponent } from './cadastro-conta/cadastro-conta.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cadastro', component: CadastroUsuarioComponent },
+  { path: 'login', component: LoginUsuarioComponent },
+  { path: 'cadastro-conta', component: CadastroContaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
