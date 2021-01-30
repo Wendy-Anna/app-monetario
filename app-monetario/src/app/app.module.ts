4a5c029fab6f0777a4ba57869530059179b9b47a
Wendy-Anna <wendyufpb@gmail.com>
2021-01-30 11:55:20 -0300

    Correçao do cadastro

--- app-monetario/src/app/app.module.ts
+++ app-monetario/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CadastroUsuarioComponent } from './cadastro-usuario/cadastro-usuario.component';
+import { CadastroUsuarioComponent } from '../cadastro-usuario/cadastro-usuario.component';
 
 @NgModule({
   declarations: [
